08015ff1b69deab4db87712cedbf90fd5e69b0fd
Nikki U <nunvericht@gmail.com>
2024-04-02 17:21:31 -0500

    Initial Commit

---
+++ CPSC5340_Final_Project/CPSC5340_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPSC5340_Final_ProjectApp.swift
+//  CPSC5340_Final_Project
+//
+//  Created by Nicholl Unvericht on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CPSC5340_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPSC5340_Final_Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CPSC5340_Final_Project
+//
+//  Created by Nicholl Unvericht on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
